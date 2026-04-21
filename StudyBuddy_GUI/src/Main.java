00578b534de438a320f00141d1990eb75344d9cb
SariElKhoury <sari.elkhoury7@gmail.com>
2026-04-21 16:06:57 -0700

    Enhance main method with account management logic
    Refactor main method to include account management and login handling.

--- StudyBuddy_GUI/src/Main.java
+++ StudyBuddy_GUI/src/Main.java
@@ -2,6 +2,18 @@ import All_GUI.GUI;
 
 public class Main {
     public static void main(String[] args) {
-        new GUI();
+        SwingUtilities.invokeLater(() -> {
+            AccountManager accountManager = new AccountManager();
+
+            if(accountManager.isLoggedIn()) {
+                //saved session found
+                System.out.print("Welcome back, " + accountManager.getCurrentUser().getUsername() + "!");
+
+                new GUI(accountManager);
+            } else {
+                //show log in page
+                new LoginPage(accountManager, () -> SwingUtilities.invokeLater(() -> new GUI(accountManager)));
+            }
+        });
     }
 }
